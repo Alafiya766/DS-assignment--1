80926fe39cf86a51da6d85ff6df9f98c90ea674f
Alafiya N <168356443+Alafiya766@users.noreply.github.com>
2024-09-02 23:09:21 +0530

    Q.no.2.c

--- Q.no.2.c
+++ Q.no.2.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Node structure for the circular queue
 typedef struct Node {
     int data;
     struct Node *next;
 } Node;
 
-// Circular Queue structure
 typedef struct CircularQueue {
     Node *front;
     Node *rear;
 } CircularQueue;
 
-// Function to create a new node
 Node* createNode(int value) {
     Node *newNode = (Node*)malloc(sizeof(Node));
     if (!newNode) {
@@ -25,20 +22,18 @@ Node* createNode(int value) {
     return newNode;
 }
 
-// Initialize the circular queue
 void initializeQueue(CircularQueue *q) {
     q->front = NULL;
     q->rear = NULL;
 }
 
-// Enqueue operation
 void enQueue(CircularQueue *q, int value) {
     Node *newNode = createNode(value);
     if (q->front == NULL) {
         // Queue is empty
         q->front = newNode;
         q->rear = newNode;
-        newNode->next = q->front; // Point to itself
+        newNode->next = q->front; 
     } else {
         // Queue is not empty
         newNode->next = q->front;
@@ -47,22 +42,19 @@ void enQueue(CircularQueue *q, int value) {
     }
 }
 
-// Dequeue operation
 int deQueue(CircularQueue *q) {
     if (q->front == NULL) {
         printf("Queue is empty\n");
-        return -1; // Indicates an error
+        return -1; 
     }
     
     Node *temp = q->front;
     int value = temp->data;
     
     if (q->front == q->rear) {
-        // Only one node in the queue
         q->front = NULL;
         q->rear = NULL;
     } else {
-        // More than one node in the queue
         q->front = q->front->next;
         q->rear->next = q->front;
     }
@@ -70,26 +62,20 @@ int deQueue(CircularQueue *q) {
     free(temp);
     return value;
 }
-
-// Get front item
 int getFront(CircularQueue *q) {
     if (q->front == NULL) {
         printf("Queue is empty\n");
-        return -1; // Indicates an error
+        return -1; 
     }
     return q->front->data;
 }
-
-// Get rear item
 int getRear(CircularQueue *q) {
     if (q->rear == NULL) {
         printf("Queue is empty\n");
-        return -1; // Indicates an error
+        return -1; 
     }
     return q->rear->data;
 }
-
-// Main function for testing the Circular Queue
 int main() {
     CircularQueue q;
     initializeQueue(&q);
